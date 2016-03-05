8a844bfcbff70ff47175577033c52a636a7627a4
Angela <Angela>
2016-03-05 13:39:55 -0800

    add procfile, and some modifications

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
+	socket.on('old messages', function(){
+		chat.find(function(err, docs){
+			socket.emit('database', docs);
+		});
+	});
+	//Welcome message and save new messages
     socket.emit('message', { message: 'Please enter a name to begin chatting' });
    	socket.on('send', function (data) {
 		var msg = new chat(data);
