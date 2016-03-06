d4c4c1be0f2d4ad52b346fe1c1781edf99e13eb4
Angela <angela.hanna.cho@gmail.com>
2016-03-05 18:53:39 -0800

    debug heroku

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ io.sockets.on('connection', function (socket) {
 		socket.emit('chatlog', {chatlog: docs});
 		console.log(docs);
 	});*/
-	socket.on('send', function (data) {
+	socket.on('message', function (data) {
 		var msg = new chat(data);
 		msg.save(function(err, mymessage){
 
